e9681d265299882dbbb1d84e9e8733eea42f2d55
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2021-11-04 15:06:08 -0300

    initial update

---
+++ src/main/java/com/learningpair/expensivetrackerapi/ExpensivetrackerapiApplication.java
@@ -0,0 +1,13 @@
+package com.learningpair.expensivetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensivetrackerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensivetrackerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningpair/expensivetrackerapi/ExpensivetrackerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningpair.expensivetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensivetrackerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
